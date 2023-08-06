33534bb5322866e9311c2b1d084ba0dae9d59a98
unknown <smartkelvin66@gmail.com>
2023-08-06 14:32:17 +0100

    navigation

--- app/tweets/[id}.tsx
+++ app/tweets/[id}.tsx
@@ -1,7 +1,16 @@
 import tweets from "@/assets/data/tweets";
+import { Text } from "react-native";
 
 import Tweet from "@/components/Tweet";
+import { useGlobalSearchParams } from "expo-router";
 
 export default function TweetScreen() {
+  const { id } = useGlobalSearchParams();
+  const tweet = tweets.find((t) => t.id === id);
+
+  if (!tweet) {
+    return <Text>tweet {id} not found</Text>;
+  }
+
   return <Tweet tweet={tweets[0]} />;
 }
